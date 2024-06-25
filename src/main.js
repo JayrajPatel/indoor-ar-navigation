cf6677a7c4f87ac6a96642afaeea5ff24e4e39cb
Jayraj Patel <jayraj.patel1407@gmail.com>
2024-06-24 20:38:41 -0400

    Set up basic Vue project structure and Three.js integration

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import * as THREE from 'three';
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+Vue.prototype.$three = THREE;
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app');
